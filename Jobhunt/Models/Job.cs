13de5cdb2932e29254d5717bf8064ddb7199318f
offic <offic@CYBERGHOST>
2025-07-13 11:01:40 +0000

    Add project files.

---
+++ Jobhunt/Models/Job.cs
@@ -0,0 +1,18 @@
+﻿namespace Jobhunt.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public string WorkTyppe { get; set; } // e.g., Remote, On-site, Hybrid
+        public string JobType { get; set; } // e.g., Full-time, Part-time, Contract
+        public string CompanyName { get; set; }
+        public string CompnayLogoUrl { get; set; } // URL to the company logo   
+        public string SalaryRange { get; set; } // e.g., "$50,000 - $70,000"
+        public DateTime PostedDate { get; set; }
+        public int JobCategoryId { get; set; } // Foreign key to JobCategory
+        public JobCategory JobCategory { get; set; } // Navigation property
+    }
+}
---
+++ Jobhunt/Models/JobCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace Jobhunt.Models
+{
+    public class JobCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Job>Jobs { get; set; }
+
+    }
+}
