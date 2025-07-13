04aad0c57d1378d7a93e860661dfd7b85380c2a9
Romeo Yeboah <qwesivybez1@gmail.com>
2025-07-13 15:07:24 +0000

    Add files via upload

---
+++ Jobhunt/Interfaces/IJobServices.cs
@@ -0,0 +1,16 @@
+﻿using Jobhunt.Models;
+namespace Jobhunt.Interfaces
+{
+    public interface IJobServices
+    {
+        Task<IEnumerable<Job>> GetAllJobsAsync();
+        Task<Job> GetJobByIdAsync(int id);
+        Task<IEnumerable<Job>> GetJobsByCategoryAsync(int categoryId);
+        Task<IEnumerable<JobCategory>> GetAllJobCategoriesAsync();
+        Task CreateJobAsync(Job job);
+        Task UpdateJobAsync(Job job);
+        Task DeleteJobAsync(int id);
+        Task<IEnumerable<Job>> SearchJobsAsync(string searchTerm);
+        Task<IEnumerable<Job>> FilterJobsAsync(string WorkType,string location, int? categoryId);
+    }
+}
